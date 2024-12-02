dbc0144a4f5f811316a8a73f6b2ebb503b85ec6c
Christina Liu <cliu18@linux-lab-077.ece.uw.edu>
2024-12-02 14:34:47 -0800

    changed TXOP for AC VO

--- contrib/Project/examples/single-bss-sld.cc
+++ contrib/Project/examples/single-bss-sld.cc
@@ -455,7 +455,7 @@ main(int argc, char* argv[])
     // Set all TXOP limit to 0
     std::list<Time> txopLimitsBE = {MicroSeconds(0)};
     std::list<Time> txopLimitsBK = {MicroSeconds(0)};
-    std::list<Time> txopLimitsVI = {MicroSeconds(3200)};
+    std::list<Time> txopLimitsVI = {MicroSeconds(1504)};
     std::list<Time> txopLimitsVO = {MicroSeconds(3200)};
     Config::Set(prefixStr + "BE_Txop/TxopLimits", AttributeContainerValue<TimeValue>(txopLimitsBE));
     Config::Set(prefixStr + "BK_Txop/TxopLimits", AttributeContainerValue<TimeValue>(txopLimitsBK));
